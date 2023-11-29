f1b8f15f909a69bc8141af39d5dfc6f206d4fc9a
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-29 15:47:40 +0100

    printed hotels info

--- index.php
+++ index.php
@@ -3,7 +3,15 @@ include __DIR__ . "/partials/header.php";
 ?>
 
 <main>
-    main
+    <?php foreach ($hotels as $hotel) { ?>
+        <div>
+            <?php
+            echo "$hotel[name] $hotel[description] $hotel[parking] $hotel[vote] $hotel[distance_to_center]"
+                ?>
+        </div>
+    <?php } ?>
+
+
 </main>
 
 <?php
