1eb94cb28c2692b93304e2b1fa926eb230af125b
giuliabaratella <giuliabaratella7@gmail.com>
2023-11-29 16:50:20 +0100

    added parking filter

--- index.php
+++ index.php
@@ -1,46 +1,60 @@
 <?php
 include __DIR__ . "/partials/header.php";
+if (isset($_GET["parking"])) {
+    $parking = $_GET["parking"];
+    if ($parking !== "all") {
+        $tempArray = [];
+        foreach ($hotels as $hotel) {
+            if ($hotel['parking'] === (bool) $parking) {
+                $tempArray[] = $hotel;
+            }
+        }
+        $hotels = $tempArray;
+    }
+
+
+}
 ?>
 
 <main>
     <div class="container">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Description</th>
-                    <th scope="col">Parking</th>
-                    <th scope="col">Vote</th>
-                    <th scope="col">Distance to center</th>
-
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($hotels as $hotel) { ?>
+        <div class="card p-2">
+            <table class="table table-striped">
+                <thead>
                     <tr>
-                        <?php foreach ($hotel as $key => $value) { ?>
-                            <td>
-                                <?php
-                                if ($key === 'parking' && $value === true) {
-                                    echo "yes";
-                                } elseif ($key === 'parking' && $value === false) {
-                                    echo 'no';
-                                } else {
-                                    echo $value;
-                                }
-                                ?>
-                            </td>
-                        <?php } ?>
+                        <th scope="col">Name</th>
+                        <th scope="col">Description</th>
+                        <th scope="col">Parking</th>
+                        <th scope="col">Vote</th>
+                        <th scope="col">Distance to center</th>
                     </tr>
-                    <!-- <div>
-                        <?php
-                        echo "$hotel[name] $hotel[description] $hotel[parking] $hotel[vote] $hotel[distance_to_center]"
-                            ?>
-                    </div> -->
-                <?php } ?>
-
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php foreach ($hotels as $hotel) { ?>
+                        <tr>
+                            <?php foreach ($hotel as $key => $value) { ?>
+                                <td>
+                                    <?php
+                                    if ($key === 'parking' && $value === true) {
+                                        echo "yes";
+                                    } elseif ($key === 'parking' && $value === false) {
+                                        echo 'no';
+                                    } else {
+                                        echo $value;
+                                    }
+                                    ?>
+                                </td>
+                            <?php } ?>
+                        </tr>
+                        <!-- <div>
+                            <?php
+                            echo "$hotel[name] $hotel[description] $hotel[parking] $hotel[vote] $hotel[distance_to_center]"
+                                ?>
+                        </div> -->
+                    <?php } ?>
+                </tbody>
+            </table>
+        </div>
     </div>
 
 
--- partials/header.php
+++ partials/header.php
@@ -10,7 +10,10 @@ include __DIR__ . "/../Model/db.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Hotel</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
+        integrity="sha512-Avb2QiuDEEvB4bZJYdft2mNjVShBftLdPG8FJ0V7irTLQ8Uo0qcPxh4Plq7G5tGm0rU+1SPhVotteLpBERwTkw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
@@ -19,6 +22,36 @@ include __DIR__ . "/../Model/db.php";
 <body>
     <header>
         <div class="container">
-            <h1>PHP Hotel</h1>
+            <nav class="navbar navbar-expand-lg bg-body-tertiary mb-5">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="index.php">PHP Hotel</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
+            </nav>
+            <div class="card p-2 mb-5">
+                <h2>Filter your research</h2>
+                <form class="d-flex" role="search" method="GET" action="index.php">
+                    <select class="form-control me-2" placeholder="Search" aria-label="Search" name="parking">
+                        <option value="all">Parking available <i class="fa-solid fa-chevron-down"></i> </option>
+                        <option value="0">No</option>
+                        <option value="1">Yes</option>
+                    </select>
+                    <button class="btn btn-primary" type="submit">Search</button>
+                </form>
+            </div>
+
         </div>
     </header>
